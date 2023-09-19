5b8a7b3d20f824dfe6d6a9c8c85143792b5ac8d5
LevelUpDv <fsuleiman.sa@gmail.com>
2023-09-19 12:55:48 +0400

    Add red border styling to unmatched password inputs

--- app.js
+++ app.js
@@ -9,12 +9,18 @@ window.addEventListener('input', () => {
     if (pwdCell1.value == "" || pwdCell2.value == "") {
         submit.disabled = true;
         msg.classList = "";
+        pwdCell1.classList = '';
+        pwdCell2.classList = '';
     } else if ( pwdCell1.value != pwdCell2.value) {
         msg.classList = 'errMsg';
+        pwdCell1.classList = 'error';
+        pwdCell2.classList = 'error';
         submit.disabled = true;
     } else {
         msg.classList = "";
         submit.disabled = false;
+        pwdCell1.classList = '';
+        pwdCell2.classList = '';
     }
 });
 
